3a7e94f92b9b1f867d9ecd127076c3d388f62fa1
Anubhav Bhardwaj <61362008+abd8126@users.noreply.github.com>
2022-07-21 23:12:07 +0530

    Add files via upload

---
+++ read_docs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import yaml
+
+with open('data.yaml') as f:
+    docs = yaml.load_all(f, Loader=yaml.FullLoader)
+
+    for doc in docs:
+
+        for k, v in doc.items():
+            print(k, "->", v)
